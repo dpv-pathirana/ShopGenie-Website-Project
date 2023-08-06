0028f34b57bf40d74633378542032168a422ddd0
dpv-pathirana <dpv-pathirana@users.noreply.github.com>
2023-08-06 23:20:00 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,26 +1,65 @@
 <?php
-
 include 'config.php';
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $pass = $_POST['password'];
+    $cpass = $_POST['cpassword'];
 
-   $name = mysqli_real_escape_string($conn, $_POST['name']);
-   $email = mysqli_real_escape_string($conn, $_POST['email']);
-   $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
-   $cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
+    // Password validation checks
+    $uppercase = preg_match('@[A-Z]@', $pass);
+    $lowercase = preg_match('@[a-z]@', $pass);
+    $number = preg_match('@[0-9]@', $pass);
+    $specialChars = preg_match('@[^\w]@', $pass);
+    $minLength = strlen($pass) >= 8;
 
-   $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+    $errors = array();
 
-   if(mysqli_num_rows($select) > 0){
-      $message[] = 'user already exist!';
-   }else{
-      mysqli_query($conn, "INSERT INTO `user_form`(name, email, password) VALUES('$name', '$email', '$pass')") or die('query failed');
-      $message[] = 'registered successfully!';
-      header('location:login.php');
-   }
+    if (!$uppercase) {
+        $errors[] = '<div class="message">Password must contain at least one uppercase letter.</div>';
+    }
 
+    if (!$lowercase) {
+        $errors[] = '<div class="message">Password must contain at least one lowercase letter.</div>';
+    }
+
+    if (!$number) {
+        $errors[] = '<div class="message">Password must contain at least one digit.</div>';
+    }
+
+    if (!$specialChars) {
+        $errors[] = '<div class="message">Password must contain at least one special character.</div>';
+    }
+
+    if (!$minLength) {
+        $errors[] = '<div class="message">Password must be at least 8 characters long.</div>';
+    }
+
+    if ($pass !== $cpass) {
+        $errors[] = '<div class="message">Passwords do not match.</div>';
+    }
+
+    if (!empty($errors)) {
+        // If there are errors, display them to the user
+        foreach ($errors as $error) {
+            echo $error . '<br>';
+        }
+    } else {
+        // If the password is valid, proceed with database operations
+        $pass = mysqli_real_escape_string($conn, md5($pass));
+
+        $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE email = '$email'") or die('query failed');
+
+        if (mysqli_num_rows($select) > 0) {
+            echo 'User already exists!';
+        } else {
+            mysqli_query($conn, "INSERT INTO `user_form`(name, email, password) VALUES('$name', '$email', '$pass')") or die('query failed');
+            echo 'Registered successfully!';
+            header('location: login.php');
+        }
+    }
 }
-
 ?>
 
 <!DOCTYPE html>
