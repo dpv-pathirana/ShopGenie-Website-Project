f9e387799751e72d18ed42addac7e3c53f033d10
dpv-pathirana <dpmailzone@gmail.com>
2023-12-06 13:51:09 +0530

    Revert "changes"
    This reverts commit aeed68fcaff0bf384e2d76f7e5ec62f0f00bc1d8.

--- components/user_header.php
+++ components/user_header.php
@@ -15,7 +15,7 @@
 
    <section class="flex">
 
-      <a href="home.php" class="logo">ShopGenie</a>
+      <a href="home.php" class="logo">ShopGenie<span>.</span></a>
 
       <nav class="navbar">
          <a href="home.php">Home</a>
