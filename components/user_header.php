aeed68fcaff0bf384e2d76f7e5ec62f0f00bc1d8
dpv-pathirana <dpmailzone@gmail.com>
2023-12-06 13:29:53 +0530

    changes

--- components/user_header.php
+++ components/user_header.php
@@ -15,7 +15,7 @@
 
    <section class="flex">
 
-      <a href="home.php" class="logo">ShopGenie<span>.</span></a>
+      <a href="home.php" class="logo">ShopGenie</a>
 
       <nav class="navbar">
          <a href="home.php">Home</a>
